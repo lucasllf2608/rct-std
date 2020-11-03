8e4359f8f9e723ea580651f8ffcbb5969c553bcd
LUCAS DE LIMA FREIRE <LUCASLF@SME.FORTALEZA.CE.GOV.BR>
2020-11-03 16:49:26 -0300

    Components and routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
+import Routes from  './routes';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-          <h1>Hello world !</h1>
-      </header>
+      <Routes></Routes>
     </div>
   );
 }
--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react';
-import api from '../../api';
+import api from '../../Api';
 import User from '../users/user';
 
 const List = () =>{
